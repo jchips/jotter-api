f1f32f7c4e0f0ea6de30e46bb74c5ab4be5e4ace
Jchips <20979096+Jchips@users.noreply.github.com>
2025-01-15 23:52:45 -0800

    added config table

---
+++ src/jotter/models/config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const ConfigModel = (sequelize, DataTypes) => {
+  return sequelize.define('Config', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    theme: {
+      type: DataTypes.ENUM('light', 'dark', 'auto'),
+      allowNull: false,
+      defaultValue: 'auto',
+    },
+    highlightActiveLine: {
+      type: DataTypes.BOOL,
+      allowNull: false,
+      defaultValue: true,
+    },
+    hideWordCount: {
+      type: DataTypes.BOOL,
+      allowNull: false,
+      defaultValue: false,
+    },
+    sort: {
+      type: DataTypes.BOOL,
+      allowNull: false,
+      defaultValue: 'createdByAsc',
+    },
+    fontSize: {
+      type: DataTypes.ENUM('12', '13', '14', '15', '16'),
+      allowNull: false,
+      defaultValue: '14',
+    },
+    gridSize: {
+      type: DataTypes.ENUM('1', '2', '3'),
+      allowNull: false,
+      defaultValue: '3',
+    },
+  });
+};
+
+module.exports = ConfigModel;
