00eeb2ddefcf2d064eabb65d68c4214800932b5a
Jchips <20979096+Jchips@users.noreply.github.com>
2025-02-06 23:12:30 -0800

    deleted unused route

--- src/jotter/routes/configs.js
+++ src/jotter/routes/configs.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 // ROUTES====================
 router.get('/', bearerAuth, getConfigs);
-router.post('/', addNewUserConfigs); // not using
 router.patch('/', bearerAuth, updateConfigs);
 
 // HANDLERS==================
@@ -16,17 +15,6 @@ async function getConfigs(req, res, next) {
   try {
     let uConfigs = await Config.findOne({ where: { userId: req.user.id } });
     res.status(200).json(uConfigs);
-  } catch (err) {
-    next(err);
-  }
-}
-
-// not using
-async function addNewUserConfigs(req, res, next) {
-  try {
-    let userId = req.body; // { userId: 1 }
-    let newUserConfigs = await Config.create({ userId });
-    res.status(201).json(newUserConfigs);
   } catch (err) {
     next(err);
   }
